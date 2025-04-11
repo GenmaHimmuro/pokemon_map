666958406bdea542d7c307331e8f79489499a521
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-04-11 07:26:06 +0500

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    id = models.AutoField(primary_key=True)
     name_ru = models.CharField(max_length=200, null=False, blank=False, verbose_name='Имя на русском')
     name_en = models.CharField(max_length=200, blank=True, verbose_name='Имя на английском')
     name_jp = models.CharField(max_length=200, blank=True, verbose_name='Имя на японском')
@@ -17,16 +16,16 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=False, blank=False,
-                                related_name='entities', verbose_name='Имя на русском')
+                                related_name='entities', verbose_name='Покемон')
     lat = models.FloatField(null=False, blank=False, verbose_name='Широта')
     lon = models.FloatField(null=False, blank=False, verbose_name='Долгота')
     appeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Появится')
     disappeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Исчезнет')
-    level = models.IntegerField(null=True, blank=True, verbose_name='Уровень')
-    health = models.IntegerField(null=True, blank=True, verbose_name='Здоровье')
-    strength = models.IntegerField(null=True, blank=True, verbose_name='Атака')
-    defence = models.IntegerField(null=True, blank=True, verbose_name='Защита')
-    stamina = models.IntegerField(null=True, blank=True, verbose_name='Выносливость')
+    level = models.PositiveBigIntegerField(null=True, blank=True, verbose_name='Уровень')
+    health = models.PositiveBigIntegerField(null=True, blank=True, verbose_name='Здоровье')
+    strength = models.PositiveBigIntegerField(null=True, blank=True, verbose_name='Атака')
+    defence = models.PositiveBigIntegerField(null=True, blank=True, verbose_name='Защита')
+    stamina = models.PositiveBigIntegerField(null=True, blank=True, verbose_name='Выносливость')
 
     def __str__(self):
         return f'{self.pokemon}'
